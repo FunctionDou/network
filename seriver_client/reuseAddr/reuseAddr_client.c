ac048180ae296ad660c2cf03bfc3c608dabf5fb2
0x0001 <1753214784@qq.com>
2019-04-23 16:52:55 +0800

    套接字选项,地址重复绑定

---
+++ seriver_client/reuseAddr/reuseAddr_client.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/epoll.h>
+#include <netinet/in.h>
+
+#define error(msg) do{	\
+	perror(msg); \
+	exit(1); \
+}while(0)
+
+#define EPOLL_MAX 1024
+
+int Socket(int opt);
+int Connect(int sock, struct sockaddr *addr, socklen_t len);
+void doClient(int sock, const char *ip, int port);
+
+int Socket(int opt){
+	int sock;
+	sock = socket(AF_INET, opt, 0);
+	if(sock <= 0)
+		error("socket");
+	return sock;
+}
+
+int Connect(int sock, struct sockaddr *addr, socklen_t len){
+	int service = connect(sock, addr, len);
+	if(service == -1)
+		error("connet");
+	return service;
+}
+
+void doClient(int service, const char *ip, int port){
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	inet_aton(ip, &addr.sin_addr);
+
+	Connect(service, (struct sockaddr *)&addr, sizeof(addr));
+
+
+	int buf[1024];
+	int n;
+	fd_set fds, tmp;
+	FD_ZERO(&fds);
+	FD_SET(STDIN_FILENO, &fds);
+	FD_SET(service, &fds);
+	int stat = 0;
+	while(1){
+		tmp = fds;
+		select(service + 1, &tmp, NULL, NULL, NULL);
+		if(FD_ISSET(STDIN_FILENO, &tmp)){
+			n = read(STDIN_FILENO, buf, sizeof(buf));
+			if(n == 0){
+				stat = 1;
+				shutdown(service, SHUT_WR);
+				FD_CLR(STDIN_FILENO, &fds);
+				continue;
+			}
+			send(service, buf, n, 0);
+		}
+		if(FD_ISSET(service, &tmp)){
+			n = recv(service, buf, sizeof(buf), 0);
+			if(n == 0){
+				if(stat == 1)
+					break;
+			}
+			write(STDOUT_FILENO, buf, n);
+		}
+	}
+	close(service);
+}
+
+int main(int argc, char *argv[]){
+	if(argc != 3){
+		fprintf(stderr, "Usge : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	int service = Socket(SOCK_STREAM);
+	doClient(service, argv[1], atoi(argv[2]));
+
+	return 0;
+}
---
+++ seriver_client/reuseAddr/reuseAddr_service.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/epoll.h>
+#include <netinet/in.h>
+
+#define error(msg) do{	\
+	perror(msg); \
+	exit(1); \
+}while(0)
+
+#define EPOLL_MAX 1024
+
+int Socket(int opt);
+void Bind(int sock, int port);
+void doservice(int server);
+int Accept(int sock, struct sockaddr_in *addr, socklen_t *len);
+
+int Socket(int opt){
+	int sock;
+	sock = socket(AF_INET, opt, 0);
+	if(sock <= 0)
+		error("socket");
+	return sock;
+}
+
+void Bind(int sock, int port){
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if(bind(sock, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+		error("bind");
+}
+
+int Accept(int sock, struct sockaddr_in *addr, socklen_t *len){
+	int client;
+	client = accept(sock, (struct sockaddr*)addr, len);
+	if(client <= 0)
+		error("accept");
+	return client;
+}
+void doservice(int service){
+	int client;
+	int epollfd;
+	char buf[1024];
+	struct epoll_event event, evets[EPOLL_MAX];
+	event.events = EPOLLIN;
+	event.data.fd = service;
+	
+	epollfd = epoll_create(2);
+	if(epoll_ctl(epollfd, EPOLL_CTL_ADD, service, &event) == -1){
+		fprintf(stderr, "epoll_ctl error\n");
+		exit(1);
+	}
+
+	int num;
+	while(1){
+		num = epoll_wait(epollfd, evets, sizeof(evets), -1);
+		if(num == 0)
+			continue;
+		else if (num < 0){
+			fprintf(stderr, "epoll_wait error");
+			exit(1);
+		}
+
+		for(int i = 0; i < num; ++i){
+			if(evets[i].data.fd == service && (evets[i].events & EPOLLIN)){
+				client = Accept(service, NULL, NULL);
+				event.events = EPOLLIN;
+				event.data.fd = client;
+				epoll_ctl(epollfd, EPOLL_CTL_ADD, client, &event);
+			}
+			else if(evets[i].events & EPOLLIN){
+				int n = recv(evets[i].data.fd, buf, sizeof(buf), 0);
+				if(n <= 0){
+					epoll_ctl(epollfd, EPOLL_CTL_DEL, evets[i].data.fd, NULL);
+					close(evets[i].data.fd);
+					fprintf(stderr, "peer close\n");
+				}
+				send(evets[i].data.fd, buf, n, 0);	
+			}
+		}
+	}
+	close(service);
+}
+
+int main(int argc, char *argv[]){
+	if(argc != 2){
+		fprintf(stderr, "Usge : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	int service = Socket(SOCK_STREAM);
+
+	int option = 1;
+	int optlen = sizeof(option);
+	setsockopt(service, SOL_SOCKET, SO_REUSEADDR, (void *)&option, optlen);
+
+	Bind(service, atoi(argv[1]));
+	if(listen(service, 1) == -1)
+		error("listen");
+	
+	doservice(service);
+
+	return 0;
+}
